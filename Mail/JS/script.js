2c647a0559ba42aa522d7813563d8c2b0c23f4f9
isma-91 <isma91@hotmail.it>
2022-10-25 16:14:43 +0200

    Add html, all basic styles and JS functions

---
+++ Mail/JS/script.js
@@ -0,0 +1,22 @@
+const eleBtn = document.querySelector(".btn");
+const eleMessage = document.querySelector(".message");
+const eleOk = document.querySelector(".ok");
+const eleDeny = document.querySelector(".deny");
+const arrMails = [
+  "bubu@ciao.com",
+  "baba@ciao.com",
+  "sasa@ciao.com",
+  "lulu@ciao.com",
+];
+
+eleBtn.addEventListener("click", function () {
+  const userMail = document.querySelector("[name=mail]").value;
+
+  if (arrMails.includes(userMail)) {
+    eleMessage.classList.add("show");
+    eleOk.classList.add("show");
+  } else {
+    eleMessage.classList.add("show");
+    eleDeny.classList.add("show");
+  }
+});
